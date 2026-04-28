315d45a0b91980d866e0eecca91ebdc9ebd730e7
Akshay Bhamare <48707136+adb2396@users.noreply.github.com>
2026-04-28 21:12:46 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 10
+float_type = 1.1
+bool_type = False
+string_type = "akshay"
+list_type = ['a', 'b']
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
