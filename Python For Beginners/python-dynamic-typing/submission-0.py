448846e32ae9a5c3b299f9ed0399ef7994831158
Akshay Bhamare <48707136+adb2396@users.noreply.github.com>
2026-04-28 21:14:12 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "abcd"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
